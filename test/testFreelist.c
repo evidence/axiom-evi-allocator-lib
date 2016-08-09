e9fbb87b5fa7cd5f6f8f8dd471451a1407b5b8f4
Bruno Morelli <b.morelli@evidence.eu.com>
2016-08-09 10:59:24 +0200

    Added freelist test

---
+++ test/testFreelist.c
@@ -0,0 +1,35 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include <inttypes.h>
+
+#include <freelist.h>
+
+int main()
+{
+	int i;
+	int err;
+
+	int vec_idx[100];
+	freelist_t fl;
+
+	char buf[108];
+	freelist_t *block_fl;
+
+	err = freelist_init(&fl, vec_idx,
+			    sizeof(vec_idx) / sizeof(vec_idx[0]));
+
+	block_fl = freelist_init_from_buffer(buf, sizeof(buf));
+
+#define NTR 20
+#define TMIN  7
+#define TMAX  14
+	for (i = 0; i < NTR; ++i) {
+		printf("%d) a:%d\n", i, freelist_alloc_idx(&fl));
+		if (i > TMIN && i < TMAX) {
+			err = freelist_free_idx(&fl, i - TMIN);
+			printf("%d) d:%d err=%d\n", i, i - TMIN, err);
+		}
+	}
+
+	return 0;
+}
