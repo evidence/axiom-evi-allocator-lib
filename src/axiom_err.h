d19d1cc340ce835ae147615d3273e7998b858ef6
Bruno Morelli <b.morelli@evidence.eu.com>
2016-08-09 10:34:32 +0200

    Updated error numbers

--- src/axiom_err.h
+++ src/axiom_err.h
@@ -3,6 +3,8 @@
 
 #define AXIOM_LMM_OK                  0
 #define AXIOM_LMM_INVALID_REGION   -100
+#define AXIOM_LMM_INVALID_BLOCK    -101
+#define AXIOM_LMM_INVALID_MEM_DESC -102
+#define AXIOM_LMM_INVALID_SIZE     -103
 
 #endif /* AXIOM_ERR_H */
-
