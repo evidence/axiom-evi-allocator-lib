c980432e4daf351802263fdaa4a6e0fd0a09e510
Bruno Morelli <b.morelli@evidence.eu.com>
2016-09-22 18:59:44 +0200

    Added support for axiom application and updated to the new kernel module behaviour

--- src/axiom_allocator.c
+++ src/axiom_allocator.c
@@ -56,9 +56,74 @@ extern unsigned long __ld_shm_info_start_addr;
 extern unsigned long __ld_shm_info_end_addr;
 */
 
-void axiom_allocator_init(uintptr_t saddr, uintptr_t eaddr)
+/*static*/ int add_region(uintptr_t saddr, uintptr_t eaddr,
+		      axiom_region_flags_t flags, axiom_region_prio_t prio)
+{
+        struct axiom_mem_dev_info request;
+	int err;
+	int fd = axiom_mem_hdlr.mem_dev_fd;
+        void *mem = (void *)saddr;
+
+        DBG("Request region [0x%"PRIxPTR"] [0x%"PRIxPTR"]\n", saddr, eaddr);
+	request.base = saddr;
+        request.size = eaddr - saddr;
+        DBG("Request region b:%ld s:%ld\n", request.base, request.size);
+
+	err = ioctl(fd, AXIOM_MEM_DEV_RESERVE_MEM, &request);
+	if (err != 0) {
+		perror("ioctl");
+		return err;
+	}
+
+	err = mprotect(mem, request.size, PROT_WRITE | PROT_READ);
+	if (err) {
+		perror("mprotect");
+		return err;
+	}
+	err = axiom_lmm_add_reg(&axiom_mem_hdlr.almm, (void *)saddr,
+				request.size, flags, prio);
+
+	DBG("axiom_lmm_add_reg err = %d\n", err);
+
+	return err;
+}
+/* Dangerous do not use*/
+/*static*/ int rem_region(uintptr_t saddr, uintptr_t eaddr)
+{
+        struct axiom_mem_dev_info request;
+	int err;
+	int fd = axiom_mem_hdlr.mem_dev_fd;
+        void *mem = (void *)saddr;
+
+	DBG("Request region [0x%"PRIxPTR"] [0x%"PRIxPTR"]\n", saddr, eaddr);
+	request.base = saddr;
+	request.size = eaddr - saddr;
+	DBG("Request region b:%ld s:%ld\n", request.base, request.size);
+
+	/*TODO: remove region inside lmm */
+
+	err = ioctl(fd, AXIOM_MEM_DEV_REVOKE_MEM, &request);
+	if (err != 0) {
+		perror("ioctl");
+		return err;
+	}
+
+	err = mprotect(mem, request.size, PROT_NONE);
+	if (err) {
+		perror("mprotect");
+		return err;
+	}
+
+	return err;
+}
+
+int axiom_allocator_init(int app_id, uintptr_t saddr, uintptr_t eaddr,
+			 uintptr_t priv_start, uintptr_t priv_end)
 {
 	int err;
+	struct axiom_mem_dev_info request;
+	char *app_id_str;
+
 /*
         unsigned long *data_start_addr = (unsigned long *)&__ld_shm_info_start_addr;
         unsigned long *data_end_addr = (unsigned long *)&__ld_shm_info_end_addr;
@@ -68,6 +133,18 @@ void axiom_allocator_init(uintptr_t saddr, uintptr_t eaddr)
 
 	err = axiom_lmm_init(&axiom_mem_hdlr.almm);
 	assert(err == AXIOM_LMM_OK);
+
+	app_id_str = getenv("AXIOM_APP_ID");
+	if (app_id_str == NULL) {
+		DBG("AXIOM_APP_ID not found\n");
+		return -1;
+	}
+
+	app_id = atoi(app_id_str);
+	if (app_id < 0) {
+		DBG("Invalid AXIOM_APP_ID\n");
+		return -1;
+	}
 
 	err = open("/dev/axiom_dev_mem0", O_RDWR);
 	DBG("open return %d\n", err);
@@ -81,9 +158,34 @@ void axiom_allocator_init(uintptr_t saddr, uintptr_t eaddr)
 	axiom_mem_hdlr.vaddr_start = saddr;
 	axiom_mem_hdlr.vaddr_end = eaddr;
 
+	request.base = saddr;
+	request.size = eaddr - saddr;
+
 	err = mprotect((void *)(saddr), size, PROT_NONE);
+	if (err)
+		return err;
+
+	err = ioctl(axiom_mem_hdlr.mem_dev_fd, AXIOM_MEM_DEV_CONFIG_VMEM, &request);
+	if (err) {
+		perror("ioctl");
+		return err;
+	} else {
+		DBG("Config B:0x%lx S:%ld\n", request.base, request.size);
+	}
+
+	err = ioctl(axiom_mem_hdlr.mem_dev_fd, AXIOM_MEM_DEV_SET_APP_ID, &app_id);
+	if (err) {
+		perror("ioctl");
+		return err;
+	}
+
+	err = add_region(priv_start, priv_end, AXIOM_PRIVATE_MEM, DEFAULT_PRIO);
+	DBG("add_private_region err = %d\n", err);
+
+	return err;
 }
 
+#if 0
 static void *axiom_request_private_region(unsigned long size)
 {
         struct axiom_mem_dev_info request;
@@ -111,7 +213,9 @@ static void *axiom_request_private_region(unsigned long size)
 
         return mem;
 }
+#endif
 
+#if 0
 static void *private_alloc_with_region(size_t nsize)
 {
 	size_t bs;
@@ -129,8 +233,7 @@ static void *private_alloc_with_region(size_t nsize)
 		return ptr;
 
 	err = axiom_lmm_add_reg(&axiom_mem_hdlr.almm, addr, bs,
-			  AXIOM_PRIVATE_MEM,
-			  DEFAULT_PRIO);
+				AXIOM_PRIVATE_MEM, DEFAULT_PRIO);
 	if (err != AXIOM_LMM_OK)
 		return ptr;
 
@@ -139,6 +242,7 @@ static void *private_alloc_with_region(size_t nsize)
 
 	return ptr;
 }
+#endif
 
 void *axiom_private_malloc(size_t sz)
 {
@@ -147,8 +251,6 @@ void *axiom_private_malloc(size_t sz)
 
 	pthread_mutex_lock(&axiom_mem_hdlr.mutex);
 	ptr = axiom_lmm_alloc(&axiom_mem_hdlr.almm, nsize, AXIOM_PRIVATE_MEM);
-	if (ptr == NULL)
-		ptr = private_alloc_with_region(nsize);
 
 	if (ptr != NULL) {
 		*((size_t *) ptr) = nsize;
@@ -160,7 +262,38 @@ void *axiom_private_malloc(size_t sz)
 	return ptr;
 }
 
-void axiom_free(void *ptr)
+static void *request_shared_memory(size_t sz)
+{
+	void *ptr;
+
+	/* TODO: to ask shared block to the server */
+	ptr = axiom_lmm_alloc(&axiom_mem_hdlr.almm, sz, AXIOM_SHARE_MEM);
+
+	return ptr;
+}
+
+void *axiom_shared_malloc(size_t sz)
+{
+	void *ptr;
+	size_t nsize = sz + sizeof(size_t);
+
+	pthread_mutex_lock(&axiom_mem_hdlr.mutex);
+	ptr = axiom_lmm_alloc(&axiom_mem_hdlr.almm, nsize, AXIOM_SHARE_MEM);
+
+	if (ptr == NULL)
+		ptr = request_shared_memory(nsize);
+
+	if (ptr != NULL) {
+		*((size_t *) ptr) = nsize;
+		ptr = (void *)((uintptr_t)ptr + sizeof(size_t));
+	}
+
+	pthread_mutex_unlock(&axiom_mem_hdlr.mutex);
+
+	return ptr;
+}
+
+static void axiom_free(void *ptr)
 {
 	char *p;
 
@@ -181,4 +314,14 @@ void axiom_free(void *ptr)
 	}
 
 	pthread_mutex_unlock(&axiom_mem_hdlr.mutex);
+}
+
+void axiom_private_free(void *ptr)
+{
+	axiom_free(ptr);
+}
+
+void axiom_shared_free(void *ptr)
+{
+	axiom_free(ptr);
 }
