ffe30f83bb87b44bdf1bd2cab02fed7bfb46b134
Stefano Garzarella <s.garzarella@evidence.eu.com>
2018-03-14 15:19:59 +0100

    axiom: update file version to v1.2

--- src/evi_err.h
+++ src/evi_err.h
@@ -1,7 +1,7 @@
 /*!
  * \file evi_err.h
  *
- * \version     v1.1
+ * \version     v1.2
  * \date        2016-09-23
  *
  * Copyright (C) 2016, Evidence Srl.
--- src/freeidx_list.h
+++ src/freeidx_list.h
@@ -1,7 +1,7 @@
 /*!
  * \file freeidx_list.h
  *
- * \version     v1.1
+ * \version     v1.2
  * \date        2016-09-23
  *
  * Copyright (C) 2016, Evidence Srl.
